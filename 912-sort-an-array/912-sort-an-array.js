f6dadd223fe3f2589da486e3a409b26bc20c18db
Argen Turatbek <59495051+argenturatbek@users.noreply.github.com>
2022-05-24 14:52:46 +0900

    Time: 217 ms (59.76%), Space: 52.8 MB (74.25%) - LeetHub

---
+++ 912-sort-an-array/912-sort-an-array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function(nums) {
+    
+        return nums.sort(function(a, b){
+                return a-b;
+        })
+        
+        
+};
