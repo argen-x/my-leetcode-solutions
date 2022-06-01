76f023310d1a938a12eafd0fcc08da81e05f3359
Argen Turatbek <59495051+argenturatbek@users.noreply.github.com>
2022-06-02 07:01:23 +0900

    Time: 2265 ms (5.24%), Space: 61.7 MB (5.12%) - LeetHub

---
+++ 485-max-consecutive-ones/485-max-consecutive-ones.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+        
+        let precount = 0;
+        let count = 0;
+        
+        for(let i = 0; i<nums.length; i++){
+                if(nums[i] === 1){
+                        precount++;
+                        if(count<=precount){
+                           count = precount;      
+                        }
+                        console.log(count);
+                        console.log(precount);
+                }else{
+                        if(count<=precount){
+                           count = precount;     
+                        }
+                        precount = 0;
+                }
+        }
+        
+        return count;
+};
