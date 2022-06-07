fac6fd8c484759008202dcd13eac5cdd477f385a
Argen Turatbek <59495051+argenturatbek@users.noreply.github.com>
2022-06-08 00:33:00 +0900

    Time: 84 ms (67.46%), Space: 44.5 MB (40.48%) - LeetHub

--- 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.js
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.js
@@ -6,15 +6,13 @@
 var minMovesToSeat = function(seats, students) {
     
     let seatSort = seats.sort((a, b) => a-b)
-    console.log(seatSort)
     let stuSort = students.sort((a, b) => a-b)
-    console.log(stuSort)
 
     let total = 0
     for(let i = 0; i<seats.length; i++){
         total+=Math.abs(seatSort[i] - stuSort[i])
     }
     
-    return Math.abs(total)
+    return total
     
 };
