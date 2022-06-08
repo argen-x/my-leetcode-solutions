bb270dc6123a5ab0edccf07bc4fd508e48dc024d
Argen Turatbek <59495051+argenturatbek@users.noreply.github.com>
2022-06-09 02:07:12 +0900

    Time: 106 ms (14.76%), Space: 44.9 MB (5.85%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.js
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.js
@@ -4,7 +4,7 @@
  */
 var freqAlphabets = function(s) {
     
-    const abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    const abc = [...('abcdefghijklmnopqrstuvwxyz')]
     
     let output = []
     
