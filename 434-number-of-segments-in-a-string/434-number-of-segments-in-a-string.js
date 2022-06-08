5433e2e5db1e8ddff838fea0ff82f96e2223cc47
Argen Turatbek <59495051+argenturatbek@users.noreply.github.com>
2022-06-09 01:35:51 +0900

    Time: 92 ms (21.60%), Space: 42 MB (45.54%) - LeetHub

--- 434-number-of-segments-in-a-string/434-number-of-segments-in-a-string.js
+++ 434-number-of-segments-in-a-string/434-number-of-segments-in-a-string.js
@@ -3,7 +3,5 @@
  * @return {number}
  */
 var countSegments = function(s) {
-    let arr = s.split(' ').filter(el => el != '')
-    
-    return arr.length
+    return (s.split(' ').filter(el => el != '')).length
 };
