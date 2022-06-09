8e432f319cbde456492ad00cdcb4600a02490f66
Argen Turatbek <59495051+argenturatbek@users.noreply.github.com>
2022-06-09 23:53:10 +0900

    Time: 214 ms (5.10%), Space: 51.3 MB (12.92%) - LeetHub

---
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ * @param {number[][]} points
+ * @return {number}
+ */
+var nearestValidPoint = function(x, y, points) {
+    
+    const m = new Map()
+    
+    for(let j = 0; j<points.length; j++)
+    {
+        let el = points[j]
+        if(el[0] === x || el[1] === y){
+            let dist = Math.abs(x-el[0]) + Math.abs(y-el[1])
+            let i = j
+            m.set(i, dist)
+        }
+    }
+
+    let mAsc = new Map([...m].sort((a, b) => a-b));
+    console.log(mAsc)
+    
+    let [firstValue] = mAsc.values()
+    let [firstKey] = mAsc.keys()
+    
+    for (const [key, value] of mAsc) {
+        console.log(key, value)
+        if(value < firstValue){            
+            firstValue = value
+            firstKey = key
+        }
+    }
+    
+    return firstKey == undefined ? -1 : firstKey
+};
