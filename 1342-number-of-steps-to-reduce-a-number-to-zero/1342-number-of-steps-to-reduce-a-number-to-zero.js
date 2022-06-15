21e6127668b41905469d743c59705525eff77a18
Argen Turatbek <59495051+argenturatbek@users.noreply.github.com>
2022-06-15 23:50:22 +0900

    Time: 82 ms (49.63%), Space: 42.3 MB (27.20%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -7,9 +7,9 @@ var numberOfSteps = function(num) {
     let count = 0
     while(num > 0){
         if(num%2 != 0){
-           num = num - 1 
+           num -= 1 
         }else{
-           num = num / 2 
+           num /= 2 
         }
         count++
     }
