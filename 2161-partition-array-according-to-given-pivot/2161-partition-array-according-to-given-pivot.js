d5e3009c2a2b55390634c96b789a6526d0adbd0f
Argen Turatbek <59495051+argenturatbek@users.noreply.github.com>
2022-06-16 23:12:49 +0900

    Time: 565 ms (15.53%), Space: 94.5 MB (38.51%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.js
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.js
@@ -4,7 +4,6 @@
  * @return {number[]}
  */
 var pivotArray = function(nums, pivot) {
-    
     let smaller = [], middle = [],  bigger = [] 
     nums.forEach((a) => { pivot > a ? smaller.push(a) : pivot == a ? middle.push(a) : bigger.push(a)})
     return smaller.concat(middle.concat(bigger))
