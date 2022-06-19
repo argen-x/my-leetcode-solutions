004b3f5843f6dcfb91e8b2723d66a4069bc1339a
Argen Turatbek <59495051+argenturatbek@users.noreply.github.com>
2022-06-20 00:15:28 +0900

    Time: 57 ms (97.40%), Space: 42 MB (51.08%) - LeetHub

--- 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.js
+++ 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.js
@@ -3,7 +3,6 @@
  * @return {boolean}
  */
 var squareIsWhite = function(coordinates) {
-    let white = [...('bdfh')]
 
-    return white.includes(coordinates[0]) ? coordinates[1] % 2 != 0 : coordinates[1] % 2 == 0
+    return [...('bdfh')].includes(coordinates[0]) ? coordinates[1] % 2 != 0 : coordinates[1] % 2 == 0
 };
