3295fee6adc8231f0a075cff24468a5175beaeef
Argen Turatbek <59495051+argenturatbek@users.noreply.github.com>
2022-07-03 01:23:17 +0900

    Time: 159 ms (20.53%), Space: 50.6 MB (30.56%) - LeetHub

---
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.js
@@ -0,0 +1,39 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var frequencySort = function(s) {
+    
+    let m = new Map()
+    let m2 = new Map()
+    for(let i = 0; i<s.length; i++){
+        if(m.has(s[i])){
+            let x = m.get(s[i])
+            x++
+            m.set(s[i], x)
+            
+            //at the same time add 
+            let inn = m2.get(s[i])
+            inn.push(s[i])
+            m2.set(s[i], inn)
+        }else{
+            m.set(s[i], 1)
+            
+            let inn = []
+            inn.push(s[i])
+            m2.set(s[i], inn)
+        }
+    }
+    
+    console.table(m2)
+    const sortM1 = new Map([...m].sort((a, b) => b[1] - a[1]));
+    
+    let out = []
+    for(let [key, value] of sortM1){
+        console.log(key)
+        out.push(m2.get(key).join(''))
+    }
+    
+    return out.join('')
+    
+};
