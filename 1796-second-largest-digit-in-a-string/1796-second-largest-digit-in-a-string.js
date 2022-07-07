dd830aad07f6a9d873f6bcb67648f442dbc91d78
Argen Turatbek <59495051+argenturatbek@users.noreply.github.com>
2022-07-08 04:22:40 +0900

    Time: 127 ms (14.82%), Space: 44.3 MB (49.63%) - LeetHub

---
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var secondHighest = function(s) {
+    let out = []
+    for(let i = 0; i<s.length; i++){
+        if(/^\d+$/.test(s[i])){
+            out.push(s[i])
+        }
+    }
+    let s2 = Array.from(new Set(out))
+    s2.sort((a, b) => b-a)
+    
+    return s2.length > 1 ? Number(s2[1]) : -1
+};
