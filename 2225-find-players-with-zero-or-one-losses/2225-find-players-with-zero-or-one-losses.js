147ead0d67c62b3578f4faf292d79728b3dede2e
Argen Turatbek <59495051+argenturatbek@users.noreply.github.com>
2022-07-09 10:33:42 +0900

    Time: 599 ms (50.00%), Space: 92.8 MB (84.67%) - LeetHub

---
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[][]} matches
+ * @return {number[][]}
+ */
+var findWinners = function(matches) {
+    let mLosers = new Map()
+    let allGamers = new Set()
+    matches.forEach(match => {
+        allGamers.add(match[0])
+        let loser = match[1]
+        allGamers.add(loser)
+        if(mLosers.has(loser)){
+            let inc = mLosers.get(loser)
+            mLosers.set(loser, inc+1)
+        }else{
+            mLosers.set(loser, 1)             
+         }
+    })
+    console.log(allGamers)
+
+    let oneLost = []
+    for(let [key, value] of mLosers){
+        if(value == 1){
+            oneLost.push(key)
+        }
+        if(allGamers.has(key)){
+            allGamers.delete(key)
+        }
+    }
+    
+    return [[...allGamers].sort((a, b) => a-b), oneLost.sort((a,b) => a - b)]
+    
+};
