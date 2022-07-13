aead15cdfc948175712624baf09776227fada045
Argen Turatbek <59495051+argenturatbek@users.noreply.github.com>
2022-07-13 21:46:44 +0900

    Time: 133 ms (20.94%), Space: 44.3 MB (68.24%) - LeetHub

---
+++ 2164-sort-even-and-odd-indices-independently/2164-sort-even-and-odd-indices-independently.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortEvenOdd = function(nums) {
+    
+    let odd = [], even = []
+    for(let i = 0; i<nums.length; i++){
+        if(i%2==0){
+            even.push(nums[i])
+        }else{
+            odd.push(nums[i])
+        }
+    }
+    
+    odd.sort((a, b) => b-a)
+    even.sort((a, b) => a-b)
+    
+    let fin = []
+    
+    let indEven = 0;
+    for(let i = 0; i<nums.length; i+=2){
+        fin[i] = even[indEven]
+        indEven++
+    }
+
+    let indOdd = 0;
+    for(let i = 1; i<nums.length; i+=2){
+        fin[i] = odd[indOdd]
+        indOdd++
+    }
+    
+    return fin
+};
