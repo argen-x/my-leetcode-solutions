a1e03f9aa4d8021b25d0e358ad7e5e84f16262e6
Argen Turatbek <59495051+argenturatbek@users.noreply.github.com>
2022-07-23 20:32:11 +0900

    Time: 190 ms (60.00%), Space: 59.5 MB (35.62%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
@@ -4,14 +4,12 @@
  */
 var longestConsecutive = function(nums) {
     
-     if(nums.length <= 1) return nums.length 
      
      let nums2 = Array.from(new Set(nums))
      nums2.sort((a, b) => a-b)
-     console.log(nums)
+
         
      if(nums2.length <= 1) return nums2.length 
-
         
      let prevLen = 1, count = 0   
      for(let i = 0; i<nums2.length-1; i++){
@@ -20,7 +18,6 @@ var longestConsecutive = function(nums) {
          }else{
                  if(count<=prevLen){
                          count = prevLen
-                      
                  }
                  prevLen = 1
          }    
