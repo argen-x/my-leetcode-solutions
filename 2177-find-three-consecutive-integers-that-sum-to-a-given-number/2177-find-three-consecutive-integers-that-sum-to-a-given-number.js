b55f39ce16bf9bb23926da7dbbfc37bde48d7927
Argen Turatbek <59495051+argenturatbek@users.noreply.github.com>
2022-07-23 20:49:24 +0900

    Time: 97 ms (48.23%), Space: 41.9 MB (86.52%) - LeetHub

--- 2177-find-three-consecutive-integers-that-sum-to-a-given-number/2177-find-three-consecutive-integers-that-sum-to-a-given-number.js
+++ 2177-find-three-consecutive-integers-that-sum-to-a-given-number/2177-find-three-consecutive-integers-that-sum-to-a-given-number.js
@@ -4,19 +4,10 @@
  */
 var sumOfThree = function(num) {
     
-     //   if(num%3 == 0){
-     //           return [num/3-1, num/3, num/3+1]
-     //   }
-     //   return []
+        if(num%3 != 0) return []    
         
-            if (num % 3 != 0) return [];
-
-    const firstNum = calcFirstNum(num);
-    return [firstNum, firstNum + 1, firstNum + 2];
-
-
-    function calcFirstNum(num) {
-        return (num - 3) / 3;
-    }
+        if(num%3 == 0){
+                return [num/3-1, num/3, num/3+1]
+        }
         
 };
