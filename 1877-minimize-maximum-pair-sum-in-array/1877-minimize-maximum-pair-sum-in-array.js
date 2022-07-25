82c2a05204989fd70f4bbcab05b34f41cb459b7a
Argen Turatbek <59495051+argenturatbek@users.noreply.github.com>
2022-07-25 22:48:04 +0900

    Time: 557 ms (35.20%), Space: 56.9 MB (49.60%) - LeetHub

---
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minPairSum = function(nums) {
+    
+    nums.sort((a, b) => a-b)
+    let low = 0, high = nums.length-1;
+    let prevValue = 0
+    while(low<high){
+        let pair = nums[low] + nums[high]    
+        prevValue = prevValue < pair ? pair : prevValue
+        low++
+        high--    
+    }    
+      
+    return prevValue
+};
