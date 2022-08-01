2375ef9c861bdec2c6414b48f0432b99cf95bdac
Argen Turatbek <59495051+argenturatbek@users.noreply.github.com>
2022-08-01 17:05:10 +0900

    Time: 73 ms (78.83%), Space: 42.5 MB (13.67%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    
+        while(n){
+                nums1.pop()
+                n--
+        }
+        
+        nums1.push(...nums2)
+        
+        return nums1.sort((a, b) => a-b)
+        
+};
