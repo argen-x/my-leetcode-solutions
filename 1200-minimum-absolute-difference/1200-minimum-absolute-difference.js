6e43330c029220619d2086e2a7e6c31dca227665
Argen Turatbek <59495051+argenturatbek@users.noreply.github.com>
2022-08-14 21:18:24 +0900

    Time: 314 ms (9.49%), Space: 71.9 MB (10.58%) - LeetHub

---
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} arr
+ * @return {number[][]}
+ */
+var minimumAbsDifference = function(arr) {
+    
+    arr.sort((a, b) => a - b)
+    let n = arr.length
+    
+    let m = new Map()
+    let minAbs = Math.abs(arr[1] - arr[0])
+    for(let i = 0; i<n-1; i++){
+        let abs = Math.abs(arr[i+1] - arr[i])
+        if(m.has(abs)){
+            let arr_x = m.get(abs)
+            arr_x.push([arr[i], arr[i+1]])
+            m.set(abs, arr_x)
+        }else{
+            let arr_x = []
+            arr_x.push([arr[i], arr[i+1]])
+            m.set(abs, arr_x)
+        }
+        if(abs<minAbs){
+            minAbs = abs
+        }
+    }
+    
+    let res = [...m.get(minAbs)].sort((a, b) => a[0] - b[0])
+    
+    return res
+    
+};
