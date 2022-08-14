bf94f10724dfc5af124bac4705ef15bed0a892b5
Argen Turatbek <59495051+argenturatbek@users.noreply.github.com>
2022-08-15 02:12:57 +0900

    Time: 128 ms (6.51%), Space: 42.7 MB (14.45%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.js
+++ 392-is-subsequence/392-is-subsequence.js
@@ -6,11 +6,9 @@
 var isSubsequence = function(s, t) {
     
     let s_arr = s.split('')
-    let t_arr = t.split('')
-    let arr = []
-    for(let i = t_arr.length-1; i>=0; i--){
+    for(let i = t.length-1; i>=0; i--){
         if(s_arr.length>0){
-            if(s_arr[s_arr.length-1] == t_arr[i]){
+            if(s_arr[s_arr.length-1] == t[i]){
                 s_arr.pop()
             }
         }
