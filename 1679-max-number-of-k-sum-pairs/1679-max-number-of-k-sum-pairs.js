ff48f762601525ddece337271e8b321fd2daa5b1
Argen Turatbek <59495051+argenturatbek@users.noreply.github.com>
2022-08-20 01:38:20 +0900

    Time: 939 ms (6.35%), Space: 141.5 MB (6.35%) - LeetHub

---
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxOperations = function(nums, k) {
+    
+    let m = new Map()
+    nums.forEach(num => m.set(num, m.get(num) == null ? 1 : m.get(num)+1))
+    console.table(m)
+    let count = 0
+    for(let num of nums){
+        if(m.has(k-num) && m.get(num)>0){
+            if(m.get(k-num) > 0){
+                m.set(num, m.get(num)-1)
+                if(m.get(k-num)>0){
+                    m.set(k-num, m.get(k-num) -1)
+                    count++
+                }
+            }
+        }
+    }
+    
+    return count
+};
