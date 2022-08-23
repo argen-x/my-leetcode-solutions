61bf7811434779486f0c7bf4bcbc89379c67b733
Argen Turatbek <59495051+argenturatbek@users.noreply.github.com>
2022-08-23 15:06:18 +0900

    Time: 129 ms (88.95%), Space: 66.8 MB (32.13%) - LeetHub

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.js
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.js
@@ -8,8 +8,7 @@ var containsNearbyDuplicate = function(nums, k) {
     let m = new Map()
     for(let i = 0; i<nums.length; i++){
         if(m.has(nums[i])){
-            let abs = Math.abs(i -  m.get(nums[i]))
-            if(abs <= k) return true
+            if(Math.abs(i -  m.get(nums[i])) <= k) return true
         }
         m.set(nums[i], i)
     }
