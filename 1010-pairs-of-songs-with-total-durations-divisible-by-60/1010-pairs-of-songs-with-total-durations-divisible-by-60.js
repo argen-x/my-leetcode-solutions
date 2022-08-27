c4cb2bdb619d2e3509e17741a0fd0c6f5026937f
Argen Turatbek <59495051+argenturatbek@users.noreply.github.com>
2022-08-27 14:33:24 +0900

    Time: 6949 ms (16.56%), Space: 44.4 MB (95.03%) - LeetHub

---
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} time
+ * @return {number}
+ */
+var numPairsDivisibleBy60 = function(time) {
+    
+    let m = new Map()
+    let res = 0
+    for(let i = 0; i<time.length; i++){
+        for(let j = i+1; j<time.length; j++){
+            let sum = time[i]+time[j]
+            if(sum % 60 == 0) res++
+        }
+    }
+    
+    return res
+};
