8f4167f7d04f08fd7f826b41082b1eadb4c34a65
Argen Turatbek <59495051+argenturatbek@users.noreply.github.com>
2022-09-11 21:47:24 +0900

    Time: 119 ms (8.66%), Space: 42.2 MB (40.13%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    
+    
+    //Implementing BubbleSort
+    let noSwaps
+    for(let i = nums.length; i>0; i--){
+        noSwaps=true
+        //Just to save time, we will not check the sorted items in the end
+        for(let j = 0; j<i-1; j++){
+            //Check two items, if not equal swap.
+            if(nums[j] > nums[j+1]){
+                let temp = nums[j]
+                nums[j] = nums[j+1]
+                nums[j+1] = temp
+                noSwaps = false
+            }
+        }
+        
+        //in order to prevent the unneccessary looping.
+        if(noSwaps) break
+    }
+    
+    return nums
+};
