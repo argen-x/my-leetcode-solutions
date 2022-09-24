178679d45bcb8ae308341c70ce6c0e9723a425c2
Argen Turatbek <59495051+argenturatbek@users.noreply.github.com>
2022-09-24 15:40:33 +0900

    Time: 117 ms (16.28%), Space: 43.1 MB (13.57%) - LeetHub

---
+++ 2409-count-days-spent-together/2409-count-days-spent-together.js
@@ -0,0 +1,51 @@
+/**
+ * @param {string} arriveAlice
+ * @param {string} leaveAlice
+ * @param {string} arriveBob
+ * @param {string} leaveBob
+ * @return {number}
+ */
+var countDaysTogether = function(arriveAlice, leaveAlice, arriveBob, leaveBob) {
+    
+    let days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    
+    let arr_mA = Number(arriveAlice.substring(0, 2)),
+        arr_dA = Number(arriveAlice.substring(3, 5)),
+        leave_mA = Number(leaveAlice.substring(0, 2)),
+        leav_dA = Number(leaveAlice.substring(3, 5))
+    
+    let arr_mB = Number(arriveBob.substring(0, 2)),
+        arr_dB = Number(arriveBob.substring(3, 5)),
+        leave_mB = Number(leaveBob.substring(0, 2)),
+        leav_dB = Number(leaveBob.substring(3, 5))
+    
+    let set_A = new Set()
+    for(let i = arr_mA; i<= leave_mA; i++){        
+        let start = i == arr_mA ? arr_dA : 1,
+            end = i == leave_mA ? leav_dA : days[i-1]
+        let month = i<10 ? `0${i}` : `${i}`
+        for(let j = start; j<=end; j++){
+            let day = j<10 ? `0${j}` : `${j}`
+            let cur_day = `${month}${day}`
+            set_A.add(cur_day)
+        }
+    }
+    
+    let count = 0
+    for(let i = arr_mB; i<= leave_mB; i++){        
+        let start = i == arr_mB ? arr_dB : 1,
+            end = i == leave_mB ? leav_dB : days[i-1]
+        let month = i<10 ? `0${i}` : `${i}`
+        for(let j = start; j<=end; j++){
+            let day = j<10 ? `0${j}` : `${j}`
+            let cur_day = `${month}${day}`
+            if(set_A.has(cur_day)) count++
+        }
+    }
+    
+    return count
+    
+    
+    
+    
+};
