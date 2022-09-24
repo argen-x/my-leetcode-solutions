2f395097b5826e98d35fcbcbca0692d33039fa6c
Argen Turatbek <59495051+argenturatbek@users.noreply.github.com>
2022-09-24 16:28:50 +0900

    Time: 107 ms (24.83%), Space: 42.2 MB (49.65%) - LeetHub

---
+++ 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} current
+ * @param {string} correct
+ * @return {number}
+ */
+var convertTime = function(current, correct) {
+
+    const [currentHour, currentMinute] = current.split(':').map(Number)
+    const [correctHour, correctMinute] = correct.split(':').map(Number)
+    
+    
+    let convert = correctHour - currentHour
+    let diff = correctMinute < currentMinute ? correctMinute + 60 - currentMinute : correctMinute - currentMinute
+    
+    while (diff >= 5) {
+        if (diff / 15 >= 1) {
+            convert += Math.floor(diff / 15)
+            diff %= 15
+            continue
+        }
+        
+        if (diff /= 5 >= 1) {
+            convert += Math.floor(diff / 5)
+            diff %= 5
+        }
+    }
+    
+    return convert += diff + (correctMinute < currentMinute ? -1 : 0)
+
+};
