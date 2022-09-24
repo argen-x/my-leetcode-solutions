c20e645d740f3bebc7ff0663ba6a04aa01da1f43
Argen Turatbek <59495051+argenturatbek@users.noreply.github.com>
2022-09-24 15:42:10 +0900

    Time: 150 ms (5.04%), Space: 42.9 MB (13.95%) - LeetHub

--- 2409-count-days-spent-together/2409-count-days-spent-together.js
+++ 2409-count-days-spent-together/2409-count-days-spent-together.js
@@ -9,6 +9,7 @@ var countDaysTogether = function(arriveAlice, leaveAlice, arriveBob, leaveBob) {
     
     let days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     
+    //convert string dates to Number, substring(startInd, endIndex) *endIndex+1
     let arr_mA = Number(arriveAlice.substring(0, 2)),
         arr_dA = Number(arriveAlice.substring(3, 5)),
         leave_mA = Number(leaveAlice.substring(0, 2)),
@@ -19,7 +20,7 @@ var countDaysTogether = function(arriveAlice, leaveAlice, arriveBob, leaveBob) {
         leave_mB = Number(leaveBob.substring(0, 2)),
         leav_dB = Number(leaveBob.substring(3, 5))
     
-    let set_A = new Set()
+    let set_A = []
     for(let i = arr_mA; i<= leave_mA; i++){        
         let start = i == arr_mA ? arr_dA : 1,
             end = i == leave_mA ? leav_dA : days[i-1]
@@ -27,7 +28,7 @@ var countDaysTogether = function(arriveAlice, leaveAlice, arriveBob, leaveBob) {
         for(let j = start; j<=end; j++){
             let day = j<10 ? `0${j}` : `${j}`
             let cur_day = `${month}${day}`
-            set_A.add(cur_day)
+            set_A.push(cur_day)
         }
     }
     
@@ -39,7 +40,7 @@ var countDaysTogether = function(arriveAlice, leaveAlice, arriveBob, leaveBob) {
         for(let j = start; j<=end; j++){
             let day = j<10 ? `0${j}` : `${j}`
             let cur_day = `${month}${day}`
-            if(set_A.has(cur_day)) count++
+            if(set_A.includes(cur_day)) count++
         }
     }
     
