f09d4327304ae9137389e3f0fe75a473d3339ff0
Argen Turatbek <59495051+argenturatbek@users.noreply.github.com>
2022-10-10 18:02:28 +0900

    Time: 79 ms (72.91%), Space: 41.3 MB (99.52%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.js
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.js
@@ -11,32 +11,11 @@
  */
 var middleNode = function(head) {
     
-    let len = getLength(head)
-    let middle = Math.floor(len/2)
-    let idx = 0
-    let curr = head
-    let newNode = null
-    while(idx < len){
-       
-        if(idx == middle){
-           newNode = curr
-           break 
-        }
-      
-        curr = curr.next
-        idx++
-    }
+    let slowptr = head, fastptr = head
+    while(fastptr != null && fastptr.next != null){
+        slowptr = slowptr.next
+        fastptr = fastptr.next.next
+    } 
+    return slowptr
     
-    return newNode
-    
-    function getLength(head){
-        //find the length
-        let len = 0
-        let curr = head
-        while (curr) {
-            len++
-            curr = curr.next
-        }
-        return len
-    }
 };
