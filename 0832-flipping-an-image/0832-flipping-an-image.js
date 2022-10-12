0687964b8ce6aa9b89e46857172cf8be6eae7576
Argen Turatbek <59495051+argenturatbek@users.noreply.github.com>
2022-10-13 00:16:31 +0900

    Time: 122 ms (28.45%), Space: 44.4 MB (15.15%) - LeetHub

---
+++ 0832-flipping-an-image/0832-flipping-an-image.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} image
+ * @return {number[][]}
+ */
+var flipAndInvertImage = function(image) {
+    
+    for(let x of image){
+        x = x.reverse()
+    }
+    
+    for(let i = 0; i<image.length; i++){
+        for(let j = 0; j<image[i].length; j++){
+            if(image[i][j] == 0){
+                image[i][j] = 1
+            }else{
+                image[i][j] = 0
+            }
+        }
+    }
+    
+    return image
+    
+};
