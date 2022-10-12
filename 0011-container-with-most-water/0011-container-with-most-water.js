4f7a13f1482c2e26e4f28aa44b86e0a174ad6f46
Argen Turatbek <59495051+argenturatbek@users.noreply.github.com>
2022-10-12 20:56:16 +0900

    Time: 145 ms (21.08%), Space: 49.4 MB (72.32%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    
+    let left = 0, right = height.length-1
+    let max = 0
+    
+    while(left < right){
+        
+        let leftLine = height[left],
+            rightLine = height[right]
+        
+        let hight = Math.min(leftLine, rightLine),
+            width = right - left
+        
+        let area = hight * width
+        
+        max = Math.max(area, max)
+        
+        if(leftLine >= rightLine){
+            right--
+        }else{
+            left++
+        }
+    }
+    
+    return max
+    
+};
