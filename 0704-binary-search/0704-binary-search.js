aec68452c9e6706413246c1ab5d00ffe055a5448
Argen Turatbek <59495051+argenturatbek@users.noreply.github.com>
2022-11-06 20:13:15 +0900

    Time: 65 ms (96.62%), Space: 44.9 MB (67.42%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let low = 0, high = nums.length-1
+    
+    while(low<=high){
+        let mid = Math.floor((low+high)/2)
+        let guess = nums[mid]
+        
+        if(guess == target){
+            return mid
+        }
+        
+        if(guess<target){
+            low = mid+1
+        }else{
+            high = mid-1
+        }
+    }
+    
+    return -1
+};
