d444a0304da8db018c8bacc800502d4d96ab3830
Argen Turatbek <59495051+argenturatbek@users.noreply.github.com>
2022-11-09 19:18:38 +0900

    Time: 1 ms (98.75%), Space: 42.8 MB (60.50%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        
+        List<Boolean> res = new ArrayList<>(candies.length);
+        int max = 0;
+        for(int candy : candies){
+            max = Math.max(max, candy);
+        }
+        
+        for(int candy : candies){
+            res.add(candy + extraCandies >= max);
+        }
+        
+        return res;
+
+    }
+}
